9b9039e5266b03a5346b90efdc0814794085596d
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2024-08-27 08:02:50 -0400

    Update lista-produtos.ts

--- server/src/lista-produtos.ts
+++ server/src/lista-produtos.ts
@@ -14,49 +14,42 @@ class ListaProdutos{
                 user: 'root',
                 database: 'test',
             });
-            const queryPreparada = connection.prepare("SELECT * from produtos");
-            return queryPreparada
-            .then((query)=>{
-                const queryExecutada = query.execute([])
-                return queryExecutada
-                .then((result)=>{
-                    const [rows, filds] = result
-                    const dados = rows as RowDataPacket[]
-                    const produtosDoBanco:Output[] = []
-                    for( let linha of dados){
-                        const id = linha.id
-                        const nome =linha.nome
-                        const descricao = linha.descricao
-                        const preco = linha.preco
-                        const imagem = linha.imagem
-                        const produto = {
-                            id,
-                            nome,
-                            descricao,
-                            preco:parseFloat(preco),
-                            imagem
-                        }
-                        produtosDoBanco.push(produto)
+            const queryPreparada = await connection.prepare("SELECT * from produtos");
+
+            const queryExecutada = queryPreparada.execute([])
+            return queryExecutada
+            .then((result)=>{
+                const [rows, filds] = result
+                const dados = rows as RowDataPacket[]
+                const produtosDoBanco:Output[] = []
+                for( let linha of dados){
+                    const id = linha.id
+                    const nome =linha.nome
+                    const descricao = linha.descricao
+                    const preco = linha.preco
+                    const imagem = linha.imagem
+                    const produto = {
+                        id,
+                        nome,
+                        descricao,
+                        preco:parseFloat(preco),
+                        imagem
                     }
-                    return produtosDoBanco
-                })
-                .catch()
-            })
-            .catch((e)=>{
-                if(e.code === 'ER_NO_SUCH_TABLE'){
-                    console.log("A tabela produtos não foi criada, "
-                    +"Crie a tabela no workbench! :D");
-                }else if(e.code==="ER_PARSE_ERROR"){
-                    console.log("Sua query está com algo errado:")
-                    console.log("Verifique: virgulas, pontos e nome de colunas.")
+                    produtosDoBanco.push(produto)
                 }
-                else{
-                    console.log("Erro query desconhecido",e);
-                }
+                return produtosDoBanco
             })
+            .catch()
         }
         catch(e){
-            if(e.code === 'ECONNREFUSED'){
+            if(e.code === 'ER_NO_SUCH_TABLE'){
+                console.log("A tabela produtos não foi criada, "
+                +"Crie a tabela no workbench! :D");
+            }else if(e.code==="ER_PARSE_ERROR"){
+                console.log("Sua query está com algo errado:")
+                console.log("Verifique: virgulas, pontos e nome de colunas.")
+            }
+            else if(e.code === 'ECONNREFUSED'){
                 console.log("LIGAR O LARAGON!! MANÉ!");
             }else if(e.code === 'ER_BAD_DB_ERROR'){
                 console.log("Deve criar o banco de DADOS {test}");
